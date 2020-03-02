5f1a69634466cbb9080d4a0c4f2398fd2b30b0fe
Folkmann <theme1256@gmail.com>
2020-03-02 13:21:15 +0100

    User object, som selv kan håndtere at sikre krav til data

--- src/main/dto/UserDTO.java
+++ src/main/dto/UserDTO.java
@@ -13,39 +13,66 @@ public class UserDTO implements Serializable{
     private List<String> roles;
     private String cpr;
     private String password;
-    //TODO Add relevant fields
+    private List<String> possible_roles = new ArrayList<String>();
 
     public UserDTO() {
         this.roles = new ArrayList<String>();
+        this.possible_roles.add("Admin");
+        this.possible_roles.add("Pharmacist");
+        this.possible_roles.add("Foreman");
+        this.possible_roles.add("Operator");
     }
 
     public int getUserId() {
         return userId;
     }
-    public void setUserId(int userId) {
-        this.userId = userId;
+    public void setUserId(int userId) throws DTOException {
+        if (userId >= 11 && userId <= 99)
+            this.userId = userId;
+        else
+            throw new DTOException("ID er ikke i den tilladte range");
     }
     public String getUserName() {
         return userName;
     }
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUserName(String userName) throws DTOException {
+        if (userName.length() >= 2  && userName.length() <= 20)
+            this.userName = userName;
+        else
+            throw new DTOException("Username er ikke i den talladt længde");
     }
     public String getIni() {
         return ini;
     }
-    public void setIni(String ini) {
-        this.ini = ini;
+    public void setIni(String ini) throws DTOException {
+        if (ini.length() >= 2  && ini.length() <= 4)
+            this.ini = ini;
+        else
+            throw new DTOException("Initialer er ikke i den talladt længde");
+    }
+    public String getCpr() {
+        return cpr;
+    }
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
     }
 
     public List<String> getRoles() {
         return roles;
     }
-    public void setRoles(List<String> roles) {
+    public void setRoles(List<String> roles) throws DTOException {
+        for (Object test : roles.toArray()) {
+            if (!possible_roles.contains(test)) {
+                throw new DTOException("Rolle " + test.toString() + " er ikke en godkendt rolle");
+            }
+        }
         this.roles = roles;
     }
 
-    public void addRole(String role){
+    public void addRole(String role) throws DTOException {
+        if (!possible_roles.contains(role)) {
+            throw new DTOException("Rolle " + role + " er ikke en godkendt rolle");
+        }
         this.roles.add(role);
     }
     /**
@@ -61,4 +88,21 @@ public class UserDTO implements Serializable{
     public String toString() {
         return "UserDTO [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", roles=" + roles + "]";
     }
+
+    public class DTOException extends Exception {
+
+        /**
+         *
+         */
+        private static final long serialVersionUID = 7355418246336739229L;
+
+        public DTOException(String msg, Throwable e) {
+            super(msg,e);
+        }
+
+        public DTOException(String msg) {
+            super(msg);
+        }
+    }
+
 }
