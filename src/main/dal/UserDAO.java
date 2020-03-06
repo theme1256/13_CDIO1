ef01c6f806f7af29457daf4a1aa24730b6757b5a
Folkmann <theme1256@gmail.com>
2020-03-06 12:51:03 +0100

    Fixes, så koden kan compile

--- src/main/dal/UserDAO.java
+++ src/main/dal/UserDAO.java
@@ -11,25 +11,6 @@ import java.util.Scanner;
 
 public class UserDAO implements IUserDAO {
 
-    public static void main(String[] args) {
-        UserDAO hej = new UserDAO();
-        UserStore userStore = new UserStore();
-
-
-
-        try {
-            userStore.loadUsers();
-        } catch ( DALException e){
-            System.out.println("Fejl");
-        }try {
-            hej.createUser();
-        } catch ( DALException e){
-            System.out.println("Fejl");
-        }
-
-    }
-
-
 
     @Override
     public UserDTO getUser(int userId) throws DALException {
@@ -42,32 +23,7 @@ public class UserDAO implements IUserDAO {
     }
 
     @Override
-    public void createUser() throws DALException {
-
-        UserDTO user = new UserDTO();
-        UserStore userStore = new UserStore();
-        PasswordGen psgen = new PasswordGen();
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Indstast ID");
-        int id = scan.nextInt();
-        System.out.println("Indstast Brugernavn");
-        String username = scan.next();
-        System.out.println("Indtast Rolle");
-        String role = scan.next();
-        System.out.println("Indstast Initialer");
-        String ini = scan.next();
-        System.out.println("Indstast Password");
-        String password = psgen.passwordGen(psgen.lowerCase+psgen.upperCase+psgen.cifre+psgen.symbols);
-        System.out.println("Dit password er: "+ password);
-        user.setUserId(id);
-        user.setUserName(username);
-        user.addRole(role);
-        user.setIni(ini);
-        user.setPassword(password);
-        userStore.data.add(user);
-        userStore.saveUsers(userStore);
-
+    public void createUser(UserDTO user) throws DALException {
 
     }
 
