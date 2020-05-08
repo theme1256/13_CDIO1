c6340ee115871c9e8ea5d6922fb103397661c2f9
christian <ChristiaNotarmaso@gmail.com>
2020-05-08 14:42:25 +0200

    Tilføjet USER ADMIN path

---
+++ src/main/api/UserService.java
@@ -0,0 +1,17 @@
+package api;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("userAdmin")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces({MediaType.APPLICATION_JSON})
+public class UserService {
+
+
+
+
+}
